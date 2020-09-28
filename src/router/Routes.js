42a5d7149c84925d60962ad311ecbda549d9ab73
Vincent <vleclerc@inow.fr>
2020-09-28 16:39:35 +0200

    lazy loading (bis)

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -3,17 +3,19 @@ import React, { Component, Suspense } from 'react'
 import { Route } from 'react-router';
 import HomePage from '../pages/home/HomePage';
 import RoomAddPage from '../pages/rooms/RoomAddPage';
-import RoomListPage from '../pages/rooms/RoomListPage';
+//import RoomListPage from '../pages/rooms/RoomListPage';
 
 //import AuthenticationPage from '../pages/authentication/AuthenticationPage';
 const authPage = React.lazy(() => import('../pages/authentication/AuthenticationPage'));
+const listPage = React.lazy(() => import('../pages/rooms/RoomListPage'));
+
 
 export class Routes extends Component {
     render() {
         return (
             <Suspense fallback={(<CircularProgress>Chargement en cours...</CircularProgress>)}>
                 <Route path="/" exact component={HomePage} />
-                <Route path="/rooms/list" exact component={RoomListPage} />
+                <Route path="/rooms/list" exact component={listPage} />
                 <Route path="/rooms/add" exact component={RoomAddPage} />
                 <Route path="/login" exact component={authPage} />
             </Suspense>
