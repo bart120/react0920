860da71f4a33fb2aaf5ea515e0e43de7f4ad9e3c
Vincent <vleclerc@inow.fr>
2020-09-30 14:26:41 +0200

    sessionStorage

--- src/core/redux/reducers/AuthenticationReducer.js
+++ src/core/redux/reducers/AuthenticationReducer.js
@@ -1,15 +1,18 @@
 import * as act from "../actions/AuthenticationActions";
 
 const INITIAL_STATE = {
-    user: null,
-    isConnected: false
+    user: JSON.parse(sessionStorage.getItem('USER')),
+    isConnected: sessionStorage.getItem('USER') !== null,
+    token: ''
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case act.AUTH_LOGIN:
+            sessionStorage.setItem('USER', JSON.stringify(action.payload));
             return { user: action.payload, isConnected: true };
         case act.AUTH_LOGOUT:
+            sessionStorage.removeItem('USER');
             return { user: null, isConnected: false };
         default:
             return state;
